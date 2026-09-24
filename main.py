9e6fca5db33db2610604b1e88f3c7899a2aadb11
CrzLusk <lucasg.cruz2000@hotmail.com>
2026-09-24 20:19:24 -0300

    Initial Tuning PID

--- main.py
+++ main.py
@@ -29,14 +29,14 @@ touch_sensor = TouchSensor(Port.S1)
 
 robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
 
-DRIVE_SPEED = 150
+DRIVE_SPEED = 200
 
 # Parâmetros PID para o seguidor de linha
 pid_params = PID_parameters(
-    proportional_gain=5.0,
+    proportional_gain=2,
     integrative_gain=0.001,
-    derivative_gain=1,
-    reference=50
+    derivative_gain=7.5,
+    reference=38.5
 )
 
 # Start and stop line following with each button press.
